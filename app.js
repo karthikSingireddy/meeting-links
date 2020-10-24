2210928e17f95e18a13802964e3cd151826ba2e6
karthikSingireddy <skarthikreddy189@gmail.com>
2020-10-23 19:17:15 -0700

    finished bookmark code & filled links for links.json

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 javascript:(
     function() {
-        let val = window.prompt("Which meeting do you want to go to?");
-        console.log(val);
+        let acct = 0;
+        function getMeeting() {
+            let val = window.prompt("Which meeting do you want to go to?");
+            console.log(val);
 
-        window.location.href = val;
+            fetch("https://raw.githubusercontent.com/karthikSingireddy/meeting-links/master/links.json")
+                .then(response => response.json())
+                .then(data => {
+                    if(!data[val]) {
+                        getMeeting();
+                    }
+                    else {
+                        window.location.href = data[val] + "?pli=1&authuser=" + acct;
+                    }
+                });
+        }
+        getMeeting();
     }
 )();
