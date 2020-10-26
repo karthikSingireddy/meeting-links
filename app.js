807f58d0023aebb71de32f9bb09a2106ffdf494b
karthikSingireddy <44452107+karthikSingireddy@users.noreply.github.com>
2020-10-26 16:43:04 -0700

    removed console logs

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ javascript:(
         let acct = 0;
         function getMeeting() {
             let val = window.prompt("Which meeting do you want to go to?");
-            console.log(val);
-
             fetch("https://raw.githubusercontent.com/karthikSingireddy/meeting-links/master/links.json")
                 .then(response => response.json())
                 .then(data => {
