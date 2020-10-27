9a910890d9beea807d4d1aedb029abad1fc3affd
karthikSingireddy <44452107+karthikSingireddy@users.noreply.github.com>
2020-10-26 17:03:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ javascript:(
         let acct = 0;
         function getMeeting() {
             let val = window.prompt("Which meeting do you want to go to?");
-            fetch("https://raw.githubusercontent.com/karthikSingireddy/meeting-links/master/links.json")
+            fetch("https://rawcdn.githack.com/karthikSingireddy/meeting-links/93fffa58fffb82fd3eb13220b96208446f843581/links.json")
                 .then(response => response.json())
                 .then(data => {
                     if(!data[val]) {
